2303605811d941de780cd03542b2098abb1b3e58
Utkarsh Verma <utkarsh874@gmail.com>
2017-06-07 16:51:26 +0530

    Update header file

--- LoRaUART.h
+++ LoRaUART.h
@@ -8,15 +8,39 @@
 	Library:: LoRaUART
 */
 
- #ifndef LoRaUART_h
- #define LoRaUART_h
+#ifndef LoRaUART_h
+#define LoRaUART_h
+
+/* _____CLASS DEFINITIONS____________________________________________________ */
+/**
+Arduino class library for communicating with LoRa module via UART.
+*/
+class LoRaUART
+{
+  public:
+  	LoRaUART();
+
+  	void initLoRa();
+  	void activateLoRa();
+  	void deactivateLoRa();
+  	void restoreDefault();
+
+
+
+  	static const string CmdType = "$CMD";
+  	char* requestCmd;
+  	static const uint8_t read = 0x00;
+  	static const uint8_t write = 0x01;
+  	uint8_t* ATcmd ;
+  	uint8_t baudrate;
+
+
+
+  	 Stream* _serial;                                             ///< reference to serial port object
 
 
 
 
 
 
-
-
-
- #endif	
+#endif	
