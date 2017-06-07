5156badc636fbe4fecca63d3aa8dac475d9cec14
Utkarsh Verma <utkarsh874@gmail.com>
2017-06-07 15:24:57 +0530

    Update source file

--- LoRaUART.cpp
+++ LoRaUART.cpp
@@ -1,3 +1,14 @@
-/*
+/**
 	@file
 	Arduino library for communication to LoRa Module via UART 
+*/
+/*
+	LoRaUART.cpp - Arduino library for communication to LoRa module via UART.
+
+	Library:: LoRaUART
+*/
+
+/* _____PROJECT INCLUDES_____________________________________________________ */
+#include "LoRaUART.h"
+
+	
