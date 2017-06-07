f334712ce03d395fc97c2d7c1d6c731cc677aab3
Utkarsh Verma <utkarsh874@gmail.com>
2017-06-07 17:42:38 +0530

    Update header file

--- LoRaUART.h
+++ LoRaUART.h
@@ -15,6 +15,7 @@
 /**
 Arduino class library for communicating with LoRa module via UART.
 */
+
 class LoRaUART
 {
   public:
@@ -24,23 +25,43 @@ class LoRaUART
   	void activateLoRa();
   	void deactivateLoRa();
   	void restoreDefault();
+  	void getBaudRate();
+  	void setBaudRate(uint32_t);
 
+	string* requestCmd;
 
+  private:
 
   	static const string CmdType = "$CMD";
-  	char* requestCmd;
   	static const uint8_t read = 0x00;
   	static const uint8_t write = 0x01;
-  	uint8_t* ATcmd ;
-  	uint8_t baudrate;
+  	static const uint8_t ATcmdRestoreDefault     = 0x00;
+  	static const uint8_t ATcmdInitLoRa           = 0x01;
+  	static const uint8_t ATcmdDeactivateLoRa     = 0x02;
+  	static const uint8_t ATcmdActivateLoRa       = 0x03;
+  	static const uint8_t ATcmdBaudRate           = 0x04;
+  	static const uint8_t ATcmdDeviceEUI          = 0x05;
+  	static const uint8_t ATcmdApplicationEUI     = 0x06;
+  	static const uint8_t ATcmdApplicationKey     = 0x07;
+  	static const uint8_t ATcmdNetworkKey         = 0x08;
+  	static const uint8_t ATcmdDeviceAddr         = 0x09;
+  	static const uint8_t ATcmdNetworkConnType    = 0x0A;
+  	static const uint8_t ATcmdNetworkID          = 0x0B;
+	static const uint8_t ATcmdADRStatus          = 0x0C;
+	static const uint8_t ATcmdUplinkAckStatus    = 0x0D;
+	static const uint8_t ATcmdDataConfirmRetries = 0x0E;
+	static const uint8_t ATcmdDefaultDataRate    = 0x0F;
+	static const uint8_t ATcmdPowerSaveMode      = 0x10;
+	static const uint8_t ATcmdClassSelection     = 0x11;
+	static const uint8_t ATcmdModuleTestCmd      = 0xFF;
+	uint8_t _RWmode;
+  	uint8_t _ATcmd ;
+  	uint32_t baudrate;
+  	uint8_t _baudrate;
 
 
 
-  	 Stream* _serial;                                             ///< reference to serial port object
 
-
-
-
-
-
+/*  	 Stream* _serial;                                             ///< reference to serial port object
+*/
 #endif	
