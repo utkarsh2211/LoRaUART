92fe6b66b2eb384c43bf3cf34a165d3a1ca9f56e
Utkarsh Verma <utkarsh874@gmail.com>
2017-06-07 16:51:06 +0530

    Update source file

--- LoRaUART.cpp
+++ LoRaUART.cpp
@@ -11,4 +11,10 @@
 /* _____PROJECT INCLUDES_____________________________________________________ */
 #include "LoRaUART.h"
 
-	
+ 	
+
+void LoRaUART::initLoRa()
+{
+requestCmd= requestCmd + CmdType + ',' + read + ',' + ATcmd + ',' + "\r\n"; 
+}	
+
